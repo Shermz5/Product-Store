64033c847613024884d481eaec9bc9e4f7b31ed6
Sherman <shermanmehlo2.0@gmail.com>
2026-02-19 16:40:07 +0200

    Setting up the schemas

---
+++ backend/models/product.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+})
