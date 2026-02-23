013d1214cd4cfcad509481c825561fc179b283e8
Sherman <shermanmehlo2.0@gmail.com>
2026-02-23 12:39:27 +0200

    Initialized PORT

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import productRoutes from "./routes/product.routes.js"
 dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT
 
 app.use(express.json())
 
 app.use("/api/products", productRoutes)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB()
-    console.log("Server started at http://localhost:5000")
+    console.log("Server started at http://localhost:" + PORT)
 })
 
